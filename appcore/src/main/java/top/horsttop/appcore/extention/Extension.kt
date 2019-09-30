33902678f43c9534705c49ecba60e6c8dfeda329
Quentin <gutianyi010@gmail.com>
2019-10-01 00:19:00 +0800

    add extension

--- appcore/src/main/java/top/horsttop/appcore/extention/Extension.kt
+++ appcore/src/main/java/top/horsttop/appcore/extention/Extension.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Point
 import android.os.Build
 import android.os.Bundle
@@ -13,8 +14,8 @@ import android.support.v4.app.ActivityOptionsCompat
 import android.support.v4.content.ContextCompat
 import android.view.View
 import android.view.WindowManager
+import android.widget.EditText
 import io.reactivex.Flowable
-import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.functions.Consumer
@@ -22,6 +23,7 @@ import io.reactivex.internal.functions.Functions
 import io.reactivex.internal.operators.flowable.FlowableInternalHelper
 import io.reactivex.schedulers.Schedulers
 import top.horsttop.appcore.ui.base.MvpView
+import java.util.regex.Pattern
 
 /**
  * Created by horsttop on 2018/4/11.
@@ -81,6 +83,16 @@ fun Activity.startActivity(view: View, clazz: Class<*>, bundle: Bundle? = null) 
     ActivityCompat.startActivity(this, intent, options.toBundle())
 }
 
+fun Activity.startActivityForResult(view: View, clazz: Class<*>, bundle: Bundle? = null) {
+    val intent = Intent(this, clazz)
+    if (null != bundle)
+        intent.putExtras(bundle)
+    val options = ActivityOptionsCompat.makeScaleUpAnimation(view,
+            view.width / 2, view.height / 2,
+            0, 0)
+    ActivityCompat.startActivityForResult(this, intent,1, options.toBundle())
+}
+
 fun Activity.startActivity(holdView: View, clazz: Class<*>, bundle: Bundle?, holdTag: String) {
     val intent = Intent(this, clazz)
     if (null != bundle)
@@ -106,7 +118,7 @@ fun <T> Flowable<T>.runOnMainThread(): Flowable<T> {
 /**
  * super subscribe
  */
-fun <T> Flowable<T>.subscribeX(onNext: Consumer<in T>, mvpView: MvpView? = null,onError:(t:Throwable) ->Unit?={}): Disposable {
+fun <T> Flowable<T>.subscribeX(onNext: Consumer<in T>, mvpView: MvpView? = null, onError:(t:Throwable) ->Unit?={}): Disposable {
     return if (mvpView == null) {
         subscribe(onNext, Consumer<Throwable> {
 
@@ -122,4 +134,88 @@ fun <T> Flowable<T>.subscribeX(onNext: Consumer<in T>, mvpView: MvpView? = null,
 
 fun <T> Flowable<T>.subscribeX(onNext: Consumer<in T>, onError: Consumer<Throwable>): Disposable {
     return subscribe(onNext, onError, Functions.EMPTY_ACTION, FlowableInternalHelper.RequestMax.INSTANCE)
+}
+
+fun onViewsClick(listener: View.OnClickListener, vararg views: View?) {
+    views.forEach {
+        it?.onClick(listener)
+    }
+}
+
+fun setEditViewText(et: EditText,text:String){
+    et.text.clear()
+    et.text.append(text)
+}
+
+fun nullBackStr(str:String?):String{
+    return if (str == null)
+        ""
+    else
+        str
+}
+
+fun getNotNullExtresString(ex:Bundle, key:String):String{
+    return if (ex.getString(key) == null)
+        ""
+    else
+        ex.getString(key)
+}
+
+
+/*
+        扩展点击事件
+     */
+fun View.onClick(listener: View.OnClickListener): View {
+    setOnClickListener(listener)
+    return this
+}
+
+/*
+    扩展点击事件，参数为方法
+ */
+fun View.onClick(method: () -> Unit): View {
+    setOnClickListener { method() }
+    return this
+}
+
+/**
+ * 判断String中是否包含中文
+ * @param str
+ * @return
+ */
+fun isContainChinese(str: String): Boolean {
+
+    val p = Pattern.compile("[\u4e00-\u9fa5]")
+    val m = p.matcher(str)
+    return if (m.find()) {
+        true
+    } else false
+}
+
+
+fun fetchVersionCode(context: Context): Int {
+
+    val pm = context.packageManager//context为当前Activity上下文
+    try {
+        val pi = pm.getPackageInfo(context.packageName, 0)
+        return pi.versionCode
+    } catch (e: PackageManager.NameNotFoundException) {
+        e.printStackTrace()
+        return 0
+    }
+
+}
+
+
+fun fetchVersionName(context: Context): String {
+
+    val pm = context.packageManager//context为当前Activity上下文
+    try {
+        val pi = pm.getPackageInfo(context.packageName, 0)
+        return pi.versionName
+    } catch (e: PackageManager.NameNotFoundException) {
+        e.printStackTrace()
+        return ""
+    }
+
 }
